54bd9fb9f6611e6ad353e83a40501cd4470694f5
giovannilamarmora <83874719+giovannilamarmora@users.noreply.github.com>
2026-04-03 15:52:07 +0000

    Deploy: 🚀 v0.2.2

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,17 +1,17 @@
 const SUPPORT_EMAIL = "support@materialhomeassistant.com";
 
 const env = {
-  login_url: "https://billing.stripe.com/p/login/test_3cI9AUexJ0kSfGB5M79fW00",
+  login_url: "https://billing.stripe.com/p/login/test_28E00kdTh5pWam2b21gnK00",
   download_free_url:
     "https://my.home-assistant.io/redirect/hacs_repository/?repository=lovelace-material-components&owner=giovannilamarmora&category=Plugin",
   /** Pro Plan */
-  pro_monthly: "https://buy.stripe.com/test_3cI9AUexJ0kSfGB5M79fW00",
-  pro_annual: "https://buy.stripe.com/test_28E4gAdtF6JgfGBcav9fW01",
-  pro_one_time: "https://buy.stripe.com/test_4gMcN60GT9Vs661dez9fW02",
+  pro_monthly: "https://buy.stripe.com/test_28E00kdTh5pWam2b21gnK00",
+  pro_annual: "https://buy.stripe.com/test_28EaEY2az4lSbq62vvgnK01",
+  //pro_one_time: "https://buy.stripe.com/test_4gMcN60GT9Vs661dez9fW02",
   /** Ultimate Plan */
-  ultimate_monthly: "https://buy.stripe.com/test_6oU6oIfBN8Robqldez9fW03",
-  ultimate_annual: "https://buy.stripe.com/test_bJe00k4X99Vs661b6r9fW04",
-  ultimate_one_time: "https://buy.stripe.com/test_cNibJ2exJffMbqleiD9fW05",
+  ultimate_monthly: "https://buy.stripe.com/test_5kQ5kE2az7y4eCib21gnK02",
+  ultimate_annual: "https://buy.stripe.com/test_28E9AUcPdcSo79Q2vvgnK03",
+  //ultimate_one_time: "https://buy.stripe.com/test_cNibJ2exJffMbqleiD9fW05",
   /** Documentation */
   documentation: "https://materialhomeassistant.com/docs/setup/overview",
   community:
